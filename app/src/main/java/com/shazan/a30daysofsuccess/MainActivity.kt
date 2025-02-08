729e69f6d7263be1be05f43d26adc1c38e693fed
shazan <shazan8178@gmail.com>
2025-02-08 12:41:35 +0530

    Update

--- app/src/main/java/com/shazan/a30daysofsuccess/MainActivity.kt
+++ app/src/main/java/com/shazan/a30daysofsuccess/MainActivity.kt
@@ -4,11 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
@@ -36,7 +33,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun QuoteApp() {
     Scaffold(
-        modifier = Modifier.windowInsetsPadding(WindowInsets.safeDrawing),
         topBar = {
             QuoteTopAppBar()
         }
